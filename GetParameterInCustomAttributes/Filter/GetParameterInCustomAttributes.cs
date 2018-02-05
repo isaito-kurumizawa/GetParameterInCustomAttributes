278754c56a151707c49e6ec8af645a8c6c2a2d17
isaito-kurumizawa <exkurumizawa@gmail.com>
2018-02-05 18:59:08 +0900

    Add files via upload

---
+++ GetParameterInCustomAttributes/Filter/GetParameterInCustomAttributes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GetParameterInCustomAttributes.Filter
+{
+    public class GetParameterAttributes : ActionFilterAttribute
+    {
+        public GetParameterAttributes()
+        {
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            // Parameters can be acquired with custom attributes.
+            var param = filterContext.ActionParameters["param"];
+            base.OnActionExecuting(filterContext);
+        }
+
+    }
+}
---
+++ GetParameterInCustomAttributes/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GetParameterInCustomAttributes.MvcApplication" Language="C#" %>
